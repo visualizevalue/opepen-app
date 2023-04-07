c7f76a9b3c14b6952d89c5f97f5459df3b8da991
jalil <jalil@1001.digital>
2023-04-07 21:10:36 +0200

    Styling setup

--- app.vue
+++ app.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>OpepenAI</h1>
   </div>
 </template>
+
+<script>
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
+
+<style lang="postcss" scoped>
+  div,
+  h1 {
+    background-color: var(--background);
+    color: var(--color);
+  }
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  css: [
+    '~/styles/index.css',
+  ],
+  postcss: {
+    plugins: {
+      'postcss-nested': {},
+      'postcss-custom-selectors': {
+        importFrom: 'styles/custom-selectors.css',
+      },
+      'postcss-custom-media': {
+        importFrom: 'styles/custom-media.css',
+      },
+      'postcss-preset-env': {
+        stage: 3,
+        features: {},
+      },
+      'autoprefixer': {},
+    },
+  },
 })
