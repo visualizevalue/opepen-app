069a3b6e454dbb2a8e4429a4966c925f0b83cfaa
jalil <jalil@1001.digital>
2023-04-20 21:49:09 +0200

    Adjust logo size on mobile

--- components/Logo.vue
+++ components/Logo.vue
@@ -15,12 +15,22 @@
       width: var(--size-7);
       height: var(--size-7);
       color: var(--gray-z-7);
+
+      @media (--max-sm) {
+        width: var(--size-6);
+        height: var(--size-6);
+      }
     }
 
     > :last-child {
       color: var(--gray-z-9);
       position: relative;
       top: 0.1em;
+
+      @media (--max-sm) {
+        height: var(--size-4);
+        width: auto;
+      }
     }
   }
 </style>
