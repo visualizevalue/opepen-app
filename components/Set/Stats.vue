f8920616e9c770a44bc1ea2b49dee900e2e34494
jalil <jalil@1001.digital>
2023-05-29 17:52:30 +0200

    Add overall stats

--- components/Set/Stats.vue
+++ components/Set/Stats.vue
@@ -4,54 +4,62 @@
     <span>Edition</span>
     <span class="th">Opt-Ins</span>
     <span class="th">Demand <small class="muted">(min {{ minDemand }}%)</small></span>
-    <span></span>
+    <span class="check"></span>
 
     <span class="th">One</span>
     <span>{{ formatNumber(edition1Count) }}</span>
     <span>{{ formatNumber(edition1Demand) }}%</span>
     <span class="check" :class="{
-      active: minDemand && edition1Demand >= minDemand,
-      closeToActive: minDemand && edition1Demand >= minDemand/2,
+      active: edition1Active,
+      closeToActive: edition1CloseToActive,
     }"><Check /></span>
 
     <span class="th">Four</span>
     <span>{{ formatNumber(edition4Count) }}</span>
     <span>{{ formatNumber(edition4Demand) }}%</span>
     <span class="check" :class="{
-      active: minDemand && edition4Demand >= minDemand,
-      closeToActive: minDemand && edition4Demand >= minDemand/2,
+      active: edition4Active,
+      closeToActive: edition4CloseToActive,
     }"><Check /></span>
 
     <span class="th">Five</span>
     <span>{{ formatNumber(edition5Count) }}</span>
     <span>{{ formatNumber(edition5Demand) }}%</span>
     <span class="check" :class="{
-      active: minDemand && edition5Demand >= minDemand,
-      closeToActive: minDemand && edition5Demand >= minDemand/2,
+      active: edition5Active,
+      closeToActive: edition5CloseToActive,
     }"><Check /></span>
 
     <span class="th">Ten</span>
     <span>{{ formatNumber(edition10Count) }}</span>
     <span>{{ formatNumber(edition10Demand) }}%</span>
     <span class="check" :class="{
-      active: minDemand && edition10Demand >= minDemand,
-      closeToActive: minDemand && edition10Demand >= minDemand/2,
+      active: edition10Active,
+      closeToActive: edition10CloseToActive,
     }"><Check /></span>
 
     <span class="th">Twenty</span>
     <span>{{ formatNumber(edition20Count) }}</span>
     <span>{{ formatNumber(edition20Demand) }}%</span>
     <span class="check" :class="{
-      active: minDemand && edition20Demand >= minDemand,
-      closeToActive: minDemand && edition20Demand >= minDemand/2,
+      active: edition20Active,
+      closeToActive: edition20CloseToActive,
     }"><Check /></span>
 
     <span class="th">Forty</span>
     <span>{{ formatNumber(edition40Count) }}</span>
     <span>{{ formatNumber(edition40Demand) }}%</span>
     <span class="check" :class="{
-      active: minDemand && edition40Demand >= minDemand,
-      closeToActive: minDemand && edition40Demand >= minDemand/2,
+      active: edition40Active,
+      closeToActive: edition40CloseToActive,
+    }"><Check /></span>
+
+    <span class="th">Overall</span>
+    <span class="th">{{ formatNumber(totalCount) }}</span>
+    <span class="th">{{ formatNumber(avgDemand) }}% <small>(<abbr title="average" class="muted">avg.</abbr>)</small></span>
+    <span class="check th" :class="{
+      active: edition40Active,
+      closeToActive: edition40CloseToActive,
     }"><Check /></span>
   </section>
 
@@ -69,6 +77,13 @@ const edition5Count = computed(() => props.set?.submitted_opepen['5']?.length)
 const edition10Count = computed(() => props.set?.submitted_opepen['10']?.length)
 const edition20Count = computed(() => props.set?.submitted_opepen['20']?.length)
 const edition40Count = computed(() => props.set?.submitted_opepen['40']?.length)
+const totalCount = computed(() => edition1Count.value +
+                                  edition4Count.value +
+                                  edition5Count.value +
+                                  edition10Count.value +
+                                  edition20Count.value +
+                                  edition40Count.value
+)
 
 // OVERSUBSCRIBED
 const minDemand = computed(() => props.set.min_subscription_percentage)
@@ -78,7 +93,27 @@ const edition5Demand = computed(() => parseInt(edition5Count.value / 5 * 100))
 const edition10Demand = computed(() => parseInt(edition10Count.value / 10 * 100))
 const edition20Demand = computed(() => parseInt(edition20Count.value / 20 * 100))
 const edition40Demand = computed(() => parseInt(edition40Count.value / 40 * 100))
+const avgDemand = computed(() => parseInt((edition1Demand.value +
+                                 edition4Demand.value +
+                                 edition5Demand.value +
+                                 edition10Demand.value +
+                                 edition20Demand.value +
+                                 edition40Demand.value) / 6)
+)
 
+const edition1Active = computed(() => minDemand.value && edition1Demand.value >= minDemand.value)
+const edition1CloseToActive = computed(() => minDemand.value && edition1Demand.value >= minDemand.value / 2)
+const edition4Active = computed(() => minDemand.value && edition4Demand.value >= minDemand.value)
+const edition4CloseToActive = computed(() => minDemand.value && edition4Demand.value >= minDemand.value / 2)
+const edition5Active = computed(() => minDemand.value && edition5Demand.value >= minDemand.value)
+const edition5CloseToActive = computed(() => minDemand.value && edition5Demand.value >= minDemand.value / 2)
+const edition10Active = computed(() => minDemand.value && edition10Demand.value >= minDemand.value)
+const edition10CloseToActive = computed(() => minDemand.value && edition10Demand.value >= minDemand.value / 2)
+const edition20Active = computed(() => minDemand.value && edition20Demand.value >= minDemand.value)
+const edition20CloseToActive = computed(() => minDemand.value && edition20Demand.value >= minDemand.value / 2)
+const edition40Active = computed(() => minDemand.value && edition40Demand.value >= minDemand.value)
+const edition40CloseToActive = computed(() => minDemand.value && edition40Demand.value >= minDemand.value / 2)
+// const overAllActive = computed(() => )
 </script>
 
 <style lang="postcss" scoped>
@@ -87,8 +122,8 @@ const edition40Demand = computed(() => parseInt(edition40Count.value / 40 * 100)
   border-top-left-radius: var(--size-1);
   border: var(--border-dark);
   background-color: var(--gray-z-1);
-  grid-template-columns: 28.5% 28.5% 28.5% 14.5%;
-  grid-template-rows: repeat(7, minmax(0, 1fr));
+  grid-template-columns: 37.5% 25% 25% 12.5%;
+  grid-template-rows: repeat(8, minmax(0, 1fr));
   gap: 0;
   overflow: hidden;
   border-color: var(--gray-z-4);
@@ -106,7 +141,16 @@ const edition40Demand = computed(() => parseInt(edition40Count.value / 40 * 100)
     line-height: var(--line-height-md);
   }
 
-  .th{
+  .nowrap {
+    white-space: nowrap;
+    flex-direction: row;
+
+    small {
+      align-self: baseline;
+    }
+  }
+
+  .th {
     background-color: var(--gray-z-2);
   }
 
@@ -116,6 +160,7 @@ const edition40Demand = computed(() => parseInt(edition40Count.value / 40 * 100)
 
   .check {
     color: var(--gray-z-5);
+    width: calc(100% + 1px);
 
     &.closeToActive {
       color: var(--yellow);
