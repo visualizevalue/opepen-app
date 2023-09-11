793ad7633edaea4ee275aba881af8cf0470a6641
jalil <jalil@1001.digital>
2023-09-11 18:31:58 +0200

    Make set replacement note collapsible

--- components/Set/ReplacementNote.client.vue
+++ components/Set/ReplacementNote.client.vue
@@ -1,19 +1,29 @@
 <template>
-  <Alert v-if="replacedSubmission" class="note">
-    <div class="preview">
-      <Image :image="replacedSubmission.edition1Image" version="sm" />
-      <Image :image="replacedSubmission.edition4Image" version="sm" />
-      <Image :image="replacedSubmission.edition5Image" version="sm" />
-      <Image :image="replacedSubmission.edition10Image" version="sm" />
-      <Image :image="replacedSubmission.edition20Image" version="sm" />
-      <Image :image="replacedSubmission.edition40Image" version="sm" />
+  <Alert v-if="replacedSubmission" class="note" :class="{ expanded }">
+    <div v-if="!expanded" class="collapsed">
+      <div class="preview">
+        <Image :image="replacedSubmission.edition1Image" version="sm" />
+      </div>
+      <p>Set "{{ replacedSubmission.name }}" replaced.</p>
     </div>
-    <div class="text">
-      <h1>Set replaced</h1>
-      <p>
-        The set below was replaced with the "{{ replacedSubmission.name }}" submission by {{ replacedSubmission.artist }} because the minimum demand threshold was not met.
-      </p>
-    </div>
+    <template v-else>
+      <div class="preview">
+        <Image :image="replacedSubmission.edition1Image" version="sm" />
+        <Image :image="replacedSubmission.edition4Image" version="sm" />
+        <Image :image="replacedSubmission.edition5Image" version="sm" />
+        <Image :image="replacedSubmission.edition10Image" version="sm" />
+        <Image :image="replacedSubmission.edition20Image" version="sm" />
+        <Image :image="replacedSubmission.edition40Image" version="sm" />
+      </div>
+      <div class="text">
+        <h1>Set replaced</h1>
+        <p>
+          The set below was replaced with the "{{ replacedSubmission.name }}" submission by {{ replacedSubmission.artist }} because the minimum demand threshold was not met.
+        </p>
+      </div>
+    </template>
+
+    <button class="expand" @click="expanded = !expanded"><Icon type="chevron-right" stroke-width="2.75" /></button>
   </Alert>
 </template>
 
@@ -22,14 +32,42 @@ const props = defineProps({
   set: Object,
   replacedSubmission: Object,
 })
+
+const expanded = ref(false)
 </script>
 
 <style lang="postcss" scoped>
 .note {
+  margin: 0;
+  position: relative;
+
+  .collapsed {
+    height: var(--size-5);
+    display: flex;
+    align-items: center;
+    gap: var(--size-4);
+
+    .preview {
+      display: block;
+      width: var(--size-5);
+      height: var(--size-5);
+
+      :deep(> .image) {
+        height: var(--size-5);
+        width: var(--size-5);
+      }
+    }
+
+    p {
+
+    }
+  }
+}
+
+.note.expanded {
   display: grid;
   grid-template-columns: 5rem 1fr;
   gap: var(--size-4);
-  margin: 0;
 
   @media (--md) {
     gap: var(--size-7);
@@ -60,8 +98,22 @@ const props = defineProps({
 }
 
 .text {
+  padding-right: var(--size-5);
+
   h1 {
     color: var(--gray-z-7);
   }
+}
+
+.expand {
+  position: absolute;
+  padding: var(--size-4);
+  top: calc(var(--size-0));
+  right: var(--size-0);
+  transform: rotate(0);
+  transition: all var(--speed);
+}
+.expanded .expand {
+  transform: rotate(90deg);
 }
 </style>
