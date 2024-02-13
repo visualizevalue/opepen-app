c4613f1610c90e72d118d474295b7742e328a882
jalil <jalil@1001.digital>
2024-02-13 04:45:09 +0100

    Fix shortened text helper

--- helpers/strings.ts
+++ helpers/strings.ts
@@ -1,4 +1,4 @@
-export const cleanText = (str: string) => str?.replace(/<[^>]*>?/gm, '')
+export const cleanText = (str: string) => str?.replace(/<[^>]*>?/gm, '') || ''
 
 export const shortenedCleanText = (str: string, length: number = 80) => {
   const txt = cleanText(str)
