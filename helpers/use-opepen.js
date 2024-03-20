5af9b3e46213ce61082d19b5a0450cfa27e669ba
jalil <jalil@1001.digital>
2024-03-20 14:55:14 +0100

    Fix how opepen are loaded

--- helpers/use-opepen.js
+++ helpers/use-opepen.js
@@ -3,16 +3,23 @@ export const useOpepen = async addresses => {
   const opepen = ref([])
   const opepenLoading = ref(false)
 
-  const fetchOpepen = async (addresses) => {
-    if (! addresses.filter(a => !!a).length) return
+  const fetchOpepen = async (addrs = addresses) => {
+    if (! addrs.filter(a => !!a).length) return
 
     opepenLoading.value = true
-    const responses = await Promise.all(addresses.map(a =>
-      $fetch(`${config.public.opepenApi}/accounts/${a}/opepen?limit=16000`)
-    ))
-    opepen.value = responses.reduce((opepen, response) => {
-      return opepen.concat(response.data)
-    }, [])
+    const responses = await Promise.all(addrs.map(async a => {
+      try {
+        return await $fetch(`${config.public.opepenApi}/accounts/${a}/opepen?limit=16000`)
+      } catch(e) {
+        return null
+      }
+    }))
+    opepen.value = responses
+      .filter(r => !! r?.data)
+      .reduce((opepen, response) => {
+        return opepen.concat(response.data)
+      }, [])
+
     opepenLoading.value = false
   }
   onMounted(() => fetchOpepen(addresses))
