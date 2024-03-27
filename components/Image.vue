83427db5898ad20c00d71d1f891ddb968874f1ea
jalil <jalil@1001.digital>
2024-03-27 15:26:05 +0000

    Quick fix video<>static

--- components/Image.vue
+++ components/Image.vue
@@ -2,7 +2,7 @@
   <article class="image" :class="{ loaded: loaded || isSVG || isVideo }" v-intersection-observer="loadImage">
     <div class="inner image">
       <iframe v-if="hasEmbed && !hasImageEmbed" :src="embedURI" frameborder="0" sandbox="allow-scripts"></iframe>
-      <video v-else-if="isVideo" :src="uri" playsinline loop autoplay muted ref="video"></video>
+      <video v-else-if="isVideo && !version" :src="uri" playsinline loop autoplay muted ref="video"></video>
       <img
         v-else-if="uri || hasImageEmbed"
         ref="imageEl"
