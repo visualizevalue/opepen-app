3f4758136c6703cfed3a1beb3034f3e2aa6ec2e6
jalil <jalil@1001.digital>
2024-04-09 10:07:33 +0200

    Fix submit page

--- pages/create/sets/submit.vue
+++ pages/create/sets/submit.vue
@@ -1,24 +1,24 @@
 <template>
-  <div>
+  <div class="submit">
     <PageHeader>
       <h1 class="breadcrumb">
         <NuxtLink to="/create/sets">Create Sets</NuxtLink>
         <span class="separator">/</span>
         <span>Submit a set</span>
       </h1>
-
-      <Alert class="inline info">
-        <p>Base your art on the Opepen Schematics.</p>
-        <Button
-          to="https://raw.githubusercontent.com/visualizevalue-dev/opepen-api/main/app/Services/OpepenSVG/opepen-schematics.svg"
-          target="_blank"
-          download
-          class="secondary"
-        >Opepen Schematics</Button>
-      </Alert>
-
-      <SetSubmissionForm />
     </PageHeader>
+
+    <Alert class="inline info">
+      <p>Base your art on the Opepen Schematics.</p>
+      <Button
+        to="https://raw.githubusercontent.com/visualizevalue-dev/opepen-api/main/app/Services/OpepenSVG/opepen-schematics.svg"
+        target="_blank"
+        download
+        class="secondary"
+      >Opepen Schematics</Button>
+    </Alert>
+
+    <SetSubmissionForm />
   </div>
 </template>
 
@@ -33,5 +33,10 @@ useMetaData({
 <style lang="postcss" scoped>
 aside {
   margin: var(--size-6) 0 0;
+}
+
+.submit {
+  max-width: var(--content-width);
+  margin: 0 auto;
 }
 </style>
