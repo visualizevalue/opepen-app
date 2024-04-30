4c5631b53311e2e148eff043d28e46ccf702258b
jalil <jalil@1001.digital>
2024-04-30 15:32:59 +0200

    Remove external ipfs dependency

--- components/Connect.server.vue
+++ components/Connect.server.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Button id="main-connect">
-      <img src="https://ipfs.vv.xyz/ipfs/QmaYoRC1pYT9iHSRd7aDhnQeysajJbpPFXnBwrGwTcr75J/1.svg" alt="Default avatar" class="avatar">
+      <img src="/solid.svg" alt="Default avatar" class="avatar">
 
       <span>Check-In</span>
     </Button>
