cccbd187ddb0a57e8ed9b1a7665c27fb41c30eb5
jalil <jalil@1001.digital>
2024-04-30 07:24:00 +0200

    Error handling for delegate cashe

--- helpers/delegate-cash.ts
+++ helpers/delegate-cash.ts
@@ -16,18 +16,23 @@ type DelegationV2 = {
 export async function fetchAddresses (account?: string): Promise<string[]> {
   if (! account) return []
 
-  const [delegationsV1, delegationsV2] = await Promise.all([
-    $fetch(`https://api.delegate.xyz/registry/v1/${account}`) as Promise<DelegationV1[]>,
-    $fetch(`https://api.delegate.xyz/registry/v2/${account}`) as Promise<DelegationV2[]>,
-  ])
+  try {
+    const [delegationsV1, delegationsV2] = await Promise.all([
+      $fetch(`https://api.delegate.xyz/registry/v1/${account}`) as Promise<DelegationV1[]>,
+      $fetch(`https://api.delegate.xyz/registry/v2/${account}`) as Promise<DelegationV2[]>,
+    ])
 
-  const filterApplicable = (d: DelegationV1|DelegationV2) => d.type === 'ALL' || (
-    d.type === 'CONTRACT' &&
-    d.contract?.toLowerCase() === `0x6339e5e072086621540d0362c4e3cea0d643e114`
-  )
+    const filterApplicable = (d: DelegationV1|DelegationV2) => d.type === 'ALL' || (
+      d.type === 'CONTRACT' &&
+      d.contract?.toLowerCase() === `0x6339e5e072086621540d0362c4e3cea0d643e114`
+    )
 
-  const applicableV1 = delegationsV1.filter(filterApplicable).map(d => d.vault.toLowerCase() === account.toLowerCase() ? d.delegate : d.vault)
-  const applicableV2 = delegationsV2.filter(filterApplicable).map(d => d.to.toLowerCase() === account.toLowerCase() ? d.from : d.to)
+    const applicableV1 = delegationsV1.filter(filterApplicable).map(d => d.vault.toLowerCase() === account.toLowerCase() ? d.delegate : d.vault)
+    const applicableV2 = delegationsV2.filter(filterApplicable).map(d => d.to.toLowerCase() === account.toLowerCase() ? d.from : d.to)
 
-  return Array.from(new Set(applicableV1.concat(applicableV2).map(a => a.toLowerCase())))
+    return Array.from(new Set(applicableV1.concat(applicableV2).map(a => a.toLowerCase())))
+  } catch (e) {
+    console.log(`Failed fetching delegations`)
+    return []
+  }
 }
