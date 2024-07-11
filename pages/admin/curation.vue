a4fe2204125a20dddc196dda339461bfdd9b52ec
jalil <jalil@1001.digital>
2024-07-11 09:36:42 +0200

    Allow admin to filter artist

--- pages/admin/curation.vue
+++ pages/admin/curation.vue
@@ -2,12 +2,17 @@
   <div>
     <div>
       <SectionTitle>Curated Art</SectionTitle>
-      <ArtistCurationList :query="query" />
+      <section>
+        <input type="text" placeholder="0x0d0e328b8fbc4a148eeae9e7b4791d7a6a0d2d07" v-model="filterAddress">
+      </section>
+      <ArtistCurationList :address="filterAddress" :query="query" />
     </div>
   </div>
 </template>
 
 <script setup>
+const filterAddress = ref(null)
+
 const query = computed(() => {
   const q = new URLSearchParams({
     sort: '-vote_score,-votes_count,id',
@@ -18,4 +23,9 @@ const query = computed(() => {
 </script>
 
 <style lang="postcss" scoped>
+section {
+  margin: var(--size-5) 0 var(--size-6);
+  display: grid;
+  gap: var(--size-5);
+}
 </style>
