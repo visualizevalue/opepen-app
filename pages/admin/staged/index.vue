4508c215adc9fe08ad722a790672af39e031ec6f
jalil <jalil@1001.digital>
2024-09-25 09:59:11 +0200

    Add staged sets page

---
+++ pages/admin/staged/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <ViewStagedSetStats />
+  </div>
+</template>
+
+<script setup>
+import { useMetaData } from '~/helpers/head'
+
+useMetaData({
+  title: 'View staged set stats',
+})
+</script>
+
+<style lang="postcss" scoped>
+</style>
