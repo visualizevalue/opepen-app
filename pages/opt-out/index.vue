71563f5bc4758a7c7a790a49e941e957596c00a3
jalil <jalil@1001.digital>
2025-01-27 23:44:34 +0100

    Set up `opt-out` index page

--- pages/opt-out/index.vue
+++ pages/opt-out/index.vue
@@ -1,7 +1,86 @@
 <template>
-  <div></div>
+  <div class="burned full">
+    <section class="about">
+      <header>
+        <h1>Consensus Met</h1>
+        <p>Opt out from the Opepen consensus protocol.</p>
+        <p>This process is irreversible, and opted out tokens will no longer be able participate in the consensus building process.</p>
+        <p>These artifacts will be issued by a separate Ethereum contract, with a maximum supply of 16,000.</p>
+      </header>
+
+      <BurnOpepenForm />
+    </section>
+
+    <section class="burn-timeline">
+      <BurnedOpepenTimeline />
+    </section>
+  </div>
 </template>
 
 <script setup>
-await navigateTo('/curate/view/permanent')
+import { useMetaData } from '~/helpers/head'
+
+useMetaData({
+  title: 'Opt-Out: Consensus Met',
+  description: 'Opt out from the Opepen consensus protocol. This process is irreversible, and opted out tokens will no longer be able participate in the consensus building process.',
+  og: 'https://opepen.nyc3.cdn.digitaloceanspaces.com/OG/og-burned.jpg',
+})
 </script>
+
+<style lang="postcss" scoped>
+.burned {
+  display: grid;
+  gap: var(--size-9);
+
+  .about {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: var(--size-5);
+    padding: var(--size-4);
+
+    h1 {
+      margin: 0 0 0.5em;
+      font-size: var(--font-title);
+    }
+
+    p {
+      margin-bottom: 0.5em;
+      color: var(--gray-z-7);
+    }
+
+    @media (--md) {
+      padding-left: 10vw;
+    }
+    @media (--xl) {
+      padding-left: 20vw;
+    }
+  }
+
+  .burn-timeline {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    @media (--md) {
+      overflow: auto;
+
+      :deep(> section) {
+        padding-right: 10vw;
+      }
+    }
+    @media (--xl) {
+      :deep(> section) {
+        padding-right: 20vw;
+      }
+    }
+  }
+
+  @media (--md) {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+    height: calc(100dvh - var(--navbar-height));
+    overflow: hidden;
+  }
+}
+</style>
