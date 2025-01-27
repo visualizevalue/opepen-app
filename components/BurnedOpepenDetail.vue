db80039a340100c3e24ab625ea3d4de3b332ac34
yougogirldoteth <linus@1001.digital>
2025-01-27 21:36:11 +0100

    new burned opepen pages

---
+++ components/BurnedOpepenDetail.vue
@@ -0,0 +1,210 @@
+<template>
+  <article class="opepen-detail">
+    <div class="wrapper">
+      <!-- Left -->
+      <div>
+        <OpepenImage :token="opepen" version="lg" />
+      </div>
+
+      <!-- Right -->
+      <div>
+        
+        <!-- Title -->
+        <div>
+          <span class="muted"> Consensus Met </span>
+          <h1>{{ opepen.name }}</h1>
+        </div>
+
+        <div class="burned-opepen">
+          <OpepenCard
+            :token="opepen.opepen"
+            :set="opepen.opepen.data?.edition || 40"
+          />
+          <div class="attributes">
+            <p>Opepen {{ opepen.opepen.token_id }} </p>
+            <p>burned on {{ formatDate(opepen.burned_at) }}</p>
+          </div>
+        </div>
+
+        <div class="row">
+          <div>
+            <span class="muted">Owned By</span>
+            <NuxtLink :to="`/${id(opepen.ownerAccount)}`">{{ opepen.ownerAccount.display }}</NuxtLink>
+          </div>
+          <span v-if="opepen.lastEvent" class="muted">
+            <a :href="`https://etherscan.io/tx/${opepen.lastEvent.transaction_hash}`">
+              Block {{ opepen.lastEvent.block_number }}
+            </a>
+          </span>
+        </div>
+      </div>
+    </div>
+
+  </article>
+</template>
+
+<script setup>
+import { id } from '~/helpers/accounts'
+import { formatDate } from '~/helpers/dates'
+
+const { opepen } = defineProps({ opepen: Object })
+</script>
+
+<style lang="postcss" scoped>
+.opepen-detail {
+  container-type: inline-size;
+  container-name: card;
+  position: relative;
+  display: grid;
+  border: var(--border);
+}
+
+.opepen-detail > .wrapper {
+  height: 100%;
+  display: grid;
+  grid-template-rows: repeat(2, 100cqw);
+  height: calc(200cqw);
+
+  @container (min-width: 32rem) {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+    grid-template-rows: auto;
+    height: calc(50cqw);
+  }
+
+  /* Image */
+  > div:first-child {
+    position: relative;
+
+    :deep(> .image) {
+      width: 100%;
+    }
+
+    :deep(> .image > .inner),
+    :deep(> .image > .image) {
+      border-radius: 0;
+      border: 0;
+    }
+
+    .patch {
+      position: absolute;
+      top: 0;
+      right: 0;
+      width: 15%;
+      height: 15%;
+      background-color: black;
+    }
+  }
+
+  /* Content */
+  > div:last-child {
+    display: flex;
+    padding: 10cqw;
+    flex-direction: column;
+    gap: 9cqw;
+    container-type: inline-size;
+    container-name: detail;
+    border-top: var(--border);
+
+    @container card (min-width: 32rem) {
+      border-top: 0;
+      border-left: var(--border);
+      padding: 4cqw;
+      gap: 3.5cqw;
+    }
+
+    > * {
+      font-size: 4cqw;
+    }
+
+    > .row {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    > div:first-child {
+
+      > div {
+        display: grid;
+        gap: 0.5em;
+        line-height: 1em;
+      }
+
+      span {
+        font-size: 1em;
+      }
+
+      span,
+      .icon {
+        display: block;
+      }
+
+      .icon {
+        width: 1em;
+        height: 1em;
+      }
+
+      svg {
+        width: 17cqw;
+        height: 17cqw;
+
+        /* @container (min-width: 32rem) {
+          width: 10cqw;
+          height: 10cqw;
+        } */
+      }
+    }
+
+    > div > h1 {
+      font-size: 10cqw;
+      margin-left: calc(-1 * 0.05em);
+      line-height: 1em;
+    }
+
+    > .burned-opepen {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      gap: 2rem;
+      align-items: flex-end;
+    }
+
+    > .attributes {
+      display: grid;
+      gap: 0.5em;
+
+      p {
+        font-size: 1em;
+        line-height: 1em;
+      }
+    }
+
+    > div:last-child {
+      align-items: flex-end;
+
+      > div:first-child {
+        display: grid;
+        gap: 0.5em;
+
+        span,
+        a {
+          display: block;
+          font-size: 1em;
+          line-height: 1em;
+        }
+      }
+    }
+  }
+}
+
+.opepen-detail > a {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  span {
+    opacity: 0;
+  }
+}
+</style>
---
+++ components/BurnedOpepenEvents.vue
@@ -0,0 +1,113 @@
+<template>
+  <PaginatedContent
+    :url="url"
+    v-slot="{ items }"
+    class="events"
+  >
+    <h2>Provenance</h2>
+    <div class="event header">
+      <span>Type</span>
+      <span>From</span>
+      <span>To</span>
+      <span>Date</span>
+    </div>
+    <div
+      v-for="event in items"
+      :key="event.id"
+      class="event"
+    >
+      <span>
+        <span v-if="event.from === ZeroAddress">Mint</span>
+        <span v-else>Transfer</span>
+      </span>
+      <NuxtLink :to="`/${id(event.fromAccount)}`">
+        <span class="visible-sm">From: </span>{{ event.fromAccount.display }}
+      </NuxtLink>
+      <NuxtLink :to="`/${id(event.toAccount)}`">
+        <span class="visible-sm">To: </span>{{ event.toAccount.display }}
+      </NuxtLink>
+      <NuxtLink :to="`https://etherscan.io/tx/${event.transaction_hash}`">
+        <span class="visible-sm">Date: </span>
+        <span>{{ formatDateTime(event.timestamp) }}</span>
+        <Icon type="external-link" />
+      </NuxtLink>
+    </div>
+  </PaginatedContent>
+</template>
+
+<script setup>
+import { ZeroAddress } from 'ethers';
+import { formatDateTime } from '~/helpers/dates'
+import { id } from '~/helpers/accounts'
+
+const { token } = defineProps({
+  token: Object,
+})
+
+const config = useRuntimeConfig()
+
+const url = `${config.public.opepenApi}/opepen/burned/${token.token_id}/events`
+</script>
+
+<style lang="postcss" scoped>
+
+.event {
+  background: var(--gray-z-1);
+  display: grid;
+  padding: var(--size-1) var(--size-2);
+  font-size: var(--font-xs);
+  border: var(--border);
+  border-radius: var(--size-2);
+  border-top-left-radius: var(--size-1);
+  margin: var(--size-2) 0;
+
+  > * {
+    color: var(--gray-z-7);
+    display: flex;
+    align-items: center;
+    gap: var(--size-2);
+
+    &:last-child {
+      @media (--md) {
+        text-align: right;
+        justify-content: flex-end;
+      }
+    }
+
+    :deep(.vue-feather) {
+      width: var(--size-4);
+      opacity: 0.5;
+    }
+  }
+
+  > a {
+    color: var(--gray-z-6);
+    transition: all var(--speed);
+
+    &:--highlight {
+      color: var(--color);
+
+      :deep(.vue-feather) {
+        opacity: 1;
+      }
+    }
+  }
+
+  @media (--md) {
+    grid-template-columns: 20% 25% 25% 30%;
+  }
+
+  &.header {
+    font-size: var(--font-sm);
+    color: var(--gray-z-5);
+    background: transparent;
+    padding-bottom: 0;
+    border: none;
+    display: none;
+
+    @media (--md) {
+      display: grid;
+    }
+  }
+}
+</style>
