8d2babbabf6248a1cf8be801e1c70f11d76623b0
jalil <jalil@1001.digital>
2025-02-06 20:42:55 +0100

    Prevent embedding for avatars

--- components/Avatar.vue
+++ components/Avatar.vue
@@ -5,6 +5,7 @@
       :image="account.pfp"
       :aspect-ratio="1"
       version="sm"
+      :auto-embed="false"
     />
     <DefaultOpepenAvatar v-else :address="account?.address" :size="size" />
   </div>
