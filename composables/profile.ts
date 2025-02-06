665cfa35e954ba222ccf21ad64030c979f29bcd0
jalil <jalil@1001.digital>
2025-02-06 20:43:28 +0100

    Cache profile

--- composables/profile.ts
+++ composables/profile.ts
@@ -3,6 +3,8 @@ const account = ref()
 export const useProfile = async (id: Ref<string>) => {
   const config = useRuntimeConfig()
 
+  if (id.value && id.value === account.value?.address) return account
+
   const url = computed(() => `${config.public.opepenApi}/accounts/${id.value}`)
   const { data } = await useFetch(url)
 
