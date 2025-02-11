44254dd3646eef0790b2976b70859e94cf8e59b8
jalil <jalil@1001.digital>
2025-02-11 20:18:44 +0100

    Navigate to currently curated set

--- pages/collect.vue
+++ pages/collect.vue
@@ -3,5 +3,11 @@
 </template>
 
 <script setup>
-navigateTo('/sets')
+definePageMeta({
+  async middleware () {
+    const { data } = await useApi(`/set-submissions/curated`)
+
+    return navigateTo(`/submissions/${data.value.submission.uuid}`)
+  }
+})
 </script>
