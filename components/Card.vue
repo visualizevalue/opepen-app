68cf276dddbd96c5d186ab8747d466aedf1eeac8
jalil <jalil@1001.digital>
2025-02-13 23:47:02 +0100

    Allow for static cards

--- components/Card.vue
+++ components/Card.vue
@@ -10,14 +10,18 @@
   transition: all var(--speed);
 
   &:--highlight {
-    transform: translateY(-0.5rem);
+    &:not(.static) {
+      transform: translateY(-0.5rem);
+    }
   }
 
   &:not(.borderless) {
     background-color: var(--gray-z-1);
 
     &:--highlight {
-      background-color: var(--gray-z-2);
+      &:not(.static) {
+        background-color: var(--gray-z-2);
+      }
     }
   }
 }
