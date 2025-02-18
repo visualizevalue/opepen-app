c3a149dc529c7dfeec51d80ee2f7f5238ebfa8ef
jalil <jalil@1001.digital>
2025-02-18 13:16:22 +0100

    Add types

--- utils/accounts.ts
+++ utils/accounts.ts
@@ -2,7 +2,7 @@ export const ADDRESS_ZERO = `0x0000000000000000000000000000000000000000`
 
 export const normalizeENS = (ens?: string) => (ens && ens.endsWith('.eth')) ? ens.slice(0, -4) : ens
 
-export const id = (account: { address: string, ens: string }) => {
+export const id = (account: Account) => {
   return account && (normalizeENS(account.ens) || account.address)
 }
 
---
+++ utils/types.ts
@@ -0,0 +1,64 @@
+export type SubmissionStats = {
+  holders: {
+    1: number
+    4: number
+    5: number
+    10: number
+    20: number
+    40: number
+    total: number
+  }
+  opepens: {
+    1: number
+    4: number
+    5: number
+    10: number
+    20: number
+    40: number
+    total: number
+  }
+  demand: {
+    1: number
+    4: number
+    5: number
+    10: number
+    20: number
+    40: number
+    total: number
+  }
+  totalHolders?: number
+}
+
+export type EditionType = 'PRINT'|'NUMBERED_PRINT'|'DYNAMIC'
+
+export type ArtistSignature = {
+  set: number,
+  name: string,
+  artist: string,
+  note?: string,
+  tx?: string,
+}
+
+export type ArtistSocials = string[]
+
+export interface Account {
+  address: string
+  ens?: string
+}
+
+export interface SetSubmission {
+  uuid: string
+  name: string
+  description?: string
+  set_id?: number
+  published_at: string|null
+  approved_at: string|null
+  starred_at: string|null
+  reveals_at: string|null
+  creatorAccount: Account
+  submission_stats?: SubmissionStats
+  edition_type: EditionType
+  reveal_strategy: string
+  reveal_block_number: string
+  artist_signature?: ArtistSignature
+}
