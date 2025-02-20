dd42f3c395cfe45b406ec69e7c7fd863d731fe50
jalil <jalil@1001.digital>
2025-02-20 22:23:09 +0100

    Add community pages

--- pages/community/artists.vue
+++ pages/community/artists.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>Artists</h1>
+  <PageFrameMd>
+    <SectionTitle>Artists</SectionTitle>
+    <PaginatedContent
+      :url="url"
+      :query="query"
+      v-slot="{ items }"
+    >
+      <ProfileCardGrid :accounts="items" />
+    </PaginatedContent>
+  </PageFrameMd>
 </template>
+
+<script setup lang="ts">
+const url = `${useConfig('opepenApi')}/accounts/artists`
+const sort = ref('-setsCount,-profileCompletion,-setSubmissionsCount,id')
+const query = computed(() => {
+  const q = new URLSearchParams(`limit=40`)
+
+  q.set('sort', sort.value)
+
+  return q.toString()
+})
+</script>
+
--- pages/community/curators.vue
+++ pages/community/curators.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>Curators</h1>
+  <PageFrameMd>
+    <SectionTitle>Curators</SectionTitle>
+    <PaginatedContent
+      :url="url"
+      :query="query"
+      v-slot="{ items }"
+    >
+      <ProfileCardGrid :accounts="items" />
+    </PaginatedContent>
+  </PageFrameMd>
 </template>
+
+<script setup lang="ts">
+const url = `${useConfig('opepenApi')}/accounts/curators`
+const sort = ref('-opepen_count,-profileCompletion,id')
+const query = computed(() => {
+  const q = new URLSearchParams(`limit=40`)
+
+  q.set('sort', sort.value)
+
+  return q.toString()
+})
+</script>
+
