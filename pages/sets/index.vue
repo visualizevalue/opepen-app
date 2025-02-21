7ffcd9b7504a455b1dff3c7e85330f7ff387d26c
jalil <jalil@1001.digital>
2025-02-21 16:50:23 +0100

    Remove the intro alerts

--- pages/sets/index.vue
+++ pages/sets/index.vue
@@ -1,17 +1,17 @@
 <template>
   <PageFrameMd>
-    <Alert type="muted" dismiss="explain-permanent-collection">
-      <h1>The Permanent Collection</h1>
-      <p>
-        The Opepen collection consists of 16.000 unique tokens on the Ethereum blockchain
-        that are revealed in "sets" (batches of 80 tokens) as more and more artists
-        contribute to the protocol and curators vote on the inclusion of art
-        in the permanent collection.
-      </p>
-      <Actions>
-        <Button to="/about/intro" class="">Learn more</Button>
-      </Actions>
-    </Alert>
+    <!-- <Alert type="muted" dismiss="explain-permanent-collection"> -->
+    <!--   <h1>The Permanent Collection</h1> -->
+    <!--   <p> -->
+    <!--     The Opepen collection consists of 16.000 unique tokens on the Ethereum blockchain -->
+    <!--     that are revealed in "sets" (batches of 80 tokens) as more and more artists -->
+    <!--     contribute to the protocol and curators vote on the inclusion of art -->
+    <!--     in the permanent collection. -->
+    <!--   </p> -->
+    <!--   <Actions> -->
+    <!--     <Button to="/about/intro" class="">Learn more</Button> -->
+    <!--   </Actions> -->
+    <!-- </Alert> -->
 
     <Progress :percent="parseInt(completeSets.length / 200 * 100)" >
       {{ completeSets.length }} / 200
