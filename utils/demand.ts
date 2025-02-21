ab391f57158d896d7f3ac0f85f477c02969024eb
jalil <jalil@1001.digital>
2025-02-21 22:21:16 +0100

    Properly calculate demand

--- utils/demand.ts
+++ utils/demand.ts
@@ -1,8 +1,14 @@
 const DEMAND_REQUIREMENT = 80
 
 export const getDemandPercentage = (submission: SetSubmission) => {
-  const totalDemand = submission.submission_stats?.demand?.total || 0
+  const totalDemand =
+    Math.min(1, submission.submission_stats?.demand[1] || 0) +
+    Math.min(4, submission.submission_stats?.demand[4] || 0) +
+    Math.min(5, submission.submission_stats?.demand[5] || 0) +
+    Math.min(10, submission.submission_stats?.demand[10] || 0) +
+    Math.min(20, submission.submission_stats?.demand[20] || 0) +
+    Math.min(40, submission.submission_stats?.demand[40] || 0)
 
-  return Math.min(100, totalDemand / DEMAND_REQUIREMENT * 100)
+  return totalDemand / DEMAND_REQUIREMENT * 100
 }
 
