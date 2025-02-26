eac6cf38c37effb8bf207518ab182106ebd44d8e
jalil <jalil@1001.digital>
2025-02-26 16:50:46 +0100

    Improve account fetching via wrapper component

---
+++ components/FetchApiAccount.client.vue
@@ -0,0 +1,19 @@
+<template>
+  <ApiAccount v-if="account" :account="account" :hide-avatar="hideAvatar" :hide-address="hideAddress" />
+</template>
+
+<script setup>
+const props = defineProps({
+  id: String,
+  hideAvatar: Boolean,
+  hideAddress: {
+    type: Boolean,
+    default: true,
+  },
+})
+
+const { data: account } = await useApi(`/accounts/${props.id}`, {
+  lazy: true
+})
+</script>
+
