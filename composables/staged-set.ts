7171cd7fd7ab46f54efed139dbe5ce26341790a6
jalil <jalil@1001.digital>
2025-02-28 17:22:20 +0100

    Improve autorefresh of staged set

--- composables/staged-set.ts
+++ composables/staged-set.ts
@@ -1,25 +1,54 @@
+const INTERVAL = 2 * 60 * 1000 // 2 minutes
+let intervalInitialized: boolean = false
+let watcherInitialized: boolean = false
 const submission = ref()
+const lastUpdated = ref()
 
 export const useStagedSet = async () => {
   const { data, refresh: reloadStagedSubmission } = await useApi('/set-submissions/curated', {
-    key: `curated-submission`,
+    key: 'curated-submission',
     dedupe: 'defer',
     immediate: false,
+    onResponse: () => {
+      lastUpdated.value = nowInSeconds()
+    }
   })
 
+  // Load initially
   if (! submission.value) await reloadStagedSubmission()
 
-  watchEffect(() => {
-    // @ts-ignore
-    if (! data.value?.submission) return
+  // Update every 2 minuts
+  if (import.meta.client && !intervalInitialized) {
+    setInterval(() => {
+      if (! submission.value) return
 
-    // @ts-ignore
-    submission.value = data.value?.submission
-  })
+      reloadStagedSubmission()
+
+      console.info(`Pinged staged set update: ${submission.value.name}`)
+    }, INTERVAL)
+
+    console.info(`Set up staged set watcher`)
+    intervalInitialized = true
+  }
+
+  // Update our cache
+  if (import.meta.client && !watcherInitialized) {
+    watchEffect(() => {
+      // @ts-ignore
+      if (! data.value?.submission) return
+
+      // @ts-ignore
+      submission.value = data.value?.submission
+
+      console.info(`Updated staged set cache: ${submission.value.name}`)
+    })
+    watcherInitialized = true
+  }
 
   return {
     submission,
     reloadStagedSubmission,
+    lastUpdated,
   }
 }
 
