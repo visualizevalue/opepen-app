1670c7959e572dac7e40a463e88e4eb3cde564f1
jalil <jalil@1001.digital>
2025-03-26 23:08:55 +0100

    Deprecate v3 components

--- .components-backup/icons/OpepenAiMedium.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <svg width="48" height="48" viewBox="0 0 48 48" fill="none" class="icon">
-    <path d="M24 2C36.1503 2 46 11.8497 46 24C46 36.1503 36.1503 46 24 46C11.8497 46 2 36.1503 2 24V2H24Z" stroke="currentColor" stroke-width="4"/>
-    <line x1="1" y1="24" x2="47" y2="24" stroke="currentColor" stroke-width="4"/>
-    <line x1="24" y1="23" x2="24" y2="47" stroke="currentColor" stroke-width="4"/>
-  </svg>
-</template>
