e4b8434093a1e9ae13022b8175bded6a6881b8c4
yougogirldoteth <linus@1001.digital>
2025-03-31 15:35:18 +0200

    Refactor useCoCreators

--- composables/creators.ts
+++ composables/creators.ts
@@ -1,10 +1,8 @@
-export const useCoCreators = (data) => (
-  [
-    data.coCreator1Account,
-    data.coCreator2Account,
-    data.coCreator3Account,
-    data.coCreator4Account,
-    data.coCreator5Account,
-  ].filter(c => !!c)
-)
-
+export const useCoCreators = (submission: any) => {
+  return computed(() => {
+    if (!submission?.coCreators) return []
+    return submission.coCreators
+      .map((c: any) => c.account)
+      .filter(Boolean)
+  })
+}
