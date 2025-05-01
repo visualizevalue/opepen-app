807002ab6459d69d1892a6ac2f114dda6b5778c3
yougogirldoteth <linus@1001.digital>
2025-05-01 19:38:27 +0200

    Allow participation also when submission is published

--- pages/submissions/[id].vue
+++ pages/submissions/[id].vue
@@ -8,7 +8,7 @@
       <SetItemsMeta :submission="submission" />
 
       <SetParticipation
-        v-if="submission.open_for_participation === true && submission.published_at === null"
+        v-if="submission.open_for_participation === true"
         :submission="submission"
         @refresh="refresh"
       />
