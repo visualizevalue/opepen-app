c644dd7c9d0d62ea0bbdf895504f78b103ab1f64
yougogirldoteth <linus@1001.digital>
2025-07-16 12:06:14 +0200

    Add opt in stats modal to all tokens

--- components/Opepen/Metadata.vue
+++ components/Opepen/Metadata.vue
@@ -59,7 +59,7 @@
         <Separator />
         <a href="#" @click.stop.prevent="downloadStatsCard" class="no-style">Info Card</a>
       </li>
-      <li v-if="opepen.revealed_at === null">
+      <li >
         <Icon type="check" />
         <span @click.stop.prevent="openModal" class="open-modal">
           {{ totalOptIns }} Opt-Ins
