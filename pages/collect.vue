02b042b740564079c2afb80e37976464a36ee553
jalil <jalil@1001.digital>
2025-08-18 19:19:24 +0200

    Redirect collect page

--- pages/collect.vue
+++ pages/collect.vue
@@ -3,17 +3,5 @@
 </template>
 
 <script setup>
-definePageMeta({
-  async middleware() {
-    const { submission, optInAvailable } = await useStagedOptIn()
-
-    if (!optInAvailable.value)
-      return navigateTo(`/submissions`, { replace: true, redirectCode: 302 })
-
-    return navigateTo(`/submissions/${submission.value.uuid}`, {
-      replace: true,
-      redirectCode: 302,
-    })
-  },
-})
+navigateTo(`/`, { replace: true, redirectCode: 302 })
 </script>
