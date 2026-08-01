265dadf58d6f71b869ef34bcb81120c8a9ce8d5f
yougogirldoteth <140090078+yougogirldoteth@users.noreply.github.com>
2026-08-01 17:31:58 +0200

    Keep autosave status width stable

--- components/Set/CompositionBoard.client.vue
+++ components/Set/CompositionBoard.client.vue
@@ -7,9 +7,11 @@
       </div>
 
       <div class="save-status" aria-live="polite">
-        <Loading v-if="saving" />
-        <Icon v-else-if="lastSaved" type="check" />
-        <span>{{ saveStatus }}</span>
+        <span class="save-status-icon" aria-hidden="true">
+          <Icon v-show="saving" class="saving-icon" type="loader" />
+          <Icon v-show="!saving && lastSaved" type="check" />
+        </span>
+        <span class="save-status-label">{{ saveStatus }}</span>
       </div>
     </header>
 
@@ -411,14 +413,32 @@ onBeforeUnmount(() => {
   @mixin ui-font;
   display: flex;
   align-items: center;
+  justify-content: flex-end;
   gap: var(--size-2);
   color: var(--muted);
   white-space: nowrap;
+}
+
+.save-status-icon {
+  display: grid;
+  flex: 0 0 var(--size-4);
+  width: var(--size-4);
+  height: var(--size-4);
 
   .icon {
+    grid-area: 1 / 1;
     width: var(--size-4);
     height: var(--size-4);
   }
+}
+
+.save-status-label {
+  width: 14ch;
+  text-align: right;
+}
+
+.saving-icon {
+  animation: spin 1s linear infinite;
 }
 
 .composition-layout {
